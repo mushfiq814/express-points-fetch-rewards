de8b1c410474a92b03dbecae976a894969add1f2
Mushfiq Mahmud <mushfiq8194@gmail.com>
2021-05-07 23:48:38 -0400

    feat(index): add routes for creating and viewing transactions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,34 @@
+import { Transaction } from './types';
 import express from 'express';
 const app = express();
 
+const transactions: Transaction[] = [];
+
+// default endpoint
 app.get('/', (req, res) => {
 	res.send('Hello World');
 });
 
-const port = 3000;
+// view transactions
+app.get('/transactions', (req, res) => {
+	res.send(transactions);
+})
+
+// add transactions
+app.post('/transactions', (req, res) => {
+	// create transaction object
+	const transaction: Transaction = {
+		payer: req.body.payer,
+		points: req.body.points,
+		timestamp: req.body.timestamp,
+	}
+
+	// add transaction to list of transactions
+	transactions.push(transaction);
+
+	// send newly created transaction as response
+	res.send(transaction);
+})
+
+const port = process.env.port || 3000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
