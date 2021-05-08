40ac812ade80391586c4fa2ef1ae79b212157a78
Mushfiq Mahmud <mushfiq8194@gmail.com>
2021-05-08 02:13:07 -0400

    style(index.ts): add comments and cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
 import { Transaction } from './types';
 import { spendPoints } from './spendPoints';
+
+// express
 import express from 'express';
 const app = express();
 app.use(express.json());
 
+// keep transactions stored in memory
 const transactions: Transaction[] = [];
 
 // default endpoint
-app.get('/', (_, res) => {
-	res.send('Hello World');
-});
+app.get('/', (_, res) => res.send('Hello World'));
 
 // view transactions
-app.get('/transactions', (_, res) => {
-	res.send(transactions);
-})
+app.get('/transactions', (_, res) => res.send(transactions));
 
 // add transactions
 app.post('/transactions', (req, res) => {
@@ -40,8 +39,13 @@ app.post('/spend', (req, res) => {
 	// calculate spending metrics based on points and transactions so far
 	const spendingMetrics = spendPoints(points, transactions);
 
+	// send spending metrics as response
 	res.send(spendingMetrics);
 })
 
+// define port value for either process environment
+// or development environment (localhost)
 const port = process.env.port || 3000;
+
+// start express server at defined port
 app.listen(port, () => console.log(`Server started on port ${port}`));
